92a8a3b6398fe5e85d63b0162958982af60ffa99
ksound22 <chrisjay967@gmail.com>
2020-12-20 02:19:00 +0100

    my best react project so far

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h3>
+        Coded By Kolade Chris Ksound <br /> Credits: Clever Programmers(Sonny
+        Sangha, Rafeh Qazi and the whole Team)
+      </h3>
+    </div>
+  );
+};
+
+export default Footer;
